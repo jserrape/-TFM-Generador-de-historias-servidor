a464dc8cc33ce1b67bd4febe02d1da867688138a
xenahort <juan.carlos.wow.95@gmail.com>
2019-02-13 12:41:02 +0100

    Añadido error 404

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from declaracionVariables import *
 def hola_mundo():
     return 'Hello, World!'
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 80))
     app.run(host='0.0.0.0', port=port, debug=True)
--- declaracionVariables.py
+++ declaracionVariables.py
@@ -1,5 +1,8 @@
+import os
+
 from flask import Flask
 from flask import send_from_directory
+from flask import render_template
 from flask import jsonify
 from flask import request
 from flask import abort
@@ -8,3 +11,8 @@ from flask import Response
 
 # Flask initialisation
 app = Flask(__name__)
+
+# HTML file directories
+HTML_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'website', 'html')
+CSS_DIR  = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'website', 'css')
+IMG_DIR  = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'website', 'img')
