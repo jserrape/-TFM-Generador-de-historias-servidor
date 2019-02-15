425e9447a8921a047329a11e4ad1c20d9141e0b3
xenahort <juan.carlos.wow.95@gmail.com>
2019-02-15 19:45:33 +0100

    Pruebas para integrar bbdd

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os, sys
 
 from declaracionVariables import *
+#from modelo import *
 
 @app.route('/', methods=['GET'])
 def get_index():
@@ -19,10 +20,22 @@ def get_status():
     response = Response(json.dumps( server_info, indent=4 ), status=200, mimetype='application/json')
     return response
 
+@app.route('/rest/historia', methods=['POST', 'PUT'])
+def POST_historia():
+    historia_dict = request.form.to_dict()
+    print('POST/PUT on historia: ' + str( historia_dict ))
+    return "Ok"
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+#@app.route('/bbdd', methods=['GET'])
+#def get_bbdd():
+#    select_employee()
+#    return "Hola mundo"
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 80))
+    #crear_modelo()
     app.run(host='0.0.0.0', port=port, debug=True)
