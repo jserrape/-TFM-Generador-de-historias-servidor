48e5bd2e4f735b06f3d7d3901e419c892e8d0bd5
xenahort <juan.carlos.wow.95@gmail.com>
2019-02-23 17:54:41 +0100

    Completado POST para registro de usuarios

--- src/server/rutas_usuario.py
+++ src/server/rutas_usuario.py
@@ -5,11 +5,12 @@ from generar_json import *
 def POST_usuario():
     usu_dict = request.form.to_dict()
     print(str( usu_dict ))
-
-
-    response = Response(json.dumps( {'status': '201'}, indent=4 ), status=201, mimetype='application/json')
-    return response
-
+    existe = existe_usuario(usu_dict['email'])
+    if existe:
+        return Response(json.dumps( {'status': '400','resultado': 'Ya existe un usuario con ese email'}, indent=4 ), status=400, mimetype='application/json')
+    else:
+        insertar_usuario(usu_dict['email'], usu_dict['nombre'], usu_dict['apellidos'], usu_dict['password'], usu_dict['imagen'])
+        return Response(json.dumps( {'status': '201','resultado': 'Se ha registrado correctamente'}, indent=4 ), status=201, mimetype='application/json')
 
 @app.route('/rest/list/usuario', methods=['GET'])
 def GET_usuarios():
@@ -20,3 +21,24 @@ def GET_usuarios():
    rows = cur.fetchall();
    print(rows)
    return render_template("list_usuario.html",rows = rows)
+
+def existe_usuario(email):
+    mail = False
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute('SELECT * FROM usuario WHERE email="' + email + '"')
+        for row in cur.fetchall():
+            mail = True
+    con.close()
+    if mail:
+        print("Usuario existe")
+    else:
+        print("Usuario NO existe")
+    return mail
+
+def insertar_usuario(email, nombre, apellidos, password, imagen):
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute("INSERT INTO usuario (email, nombre, apellidos, password, imagen) VALUES (?,?,?,?,?)",(email, nombre, apellidos, password, imagen) )
+        con.commit()
+    con.close()
