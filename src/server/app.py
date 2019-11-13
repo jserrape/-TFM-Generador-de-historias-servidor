eae4fb69914fc529c04d6c9db708852a07bd158e
Juan Carlos Serrano Pérez <juan.carlos.serrano.perez@everis.com>
2019-11-13 12:18:04 +0100

    rutas básicas acabadas

--- src/server/app.py
+++ src/server/app.py
@@ -94,7 +94,59 @@ def get_tarea(id_tarea):
 
     return respons
 
+@app.route('/list/historial/<id_historial>', methods=['GET'])
+def get_historial_id(id_historial):
+    respons = {}
+    respons['status'] = 'OK'
+    respons['ruta'] = '/list/historial/id'
 
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute('SELECT * FROM historial WHERE id="' + id_historial + '"')
+        data_max = []
+        for row in cur.fetchall():
+            data_min = {}
+            data_min['id'] = row[0]
+            data_min['id_tarea'] = row[1]
+            data_min['time'] = row[2]
+            data_min['realizada'] = row[3]
+            data_max.append(data_min)
+    con.close()
+
+    respons['tareas'] = json.dumps(data_max)
+
+    respons = jsonify(respons)
+    respons.status_code = 201
+
+    return respons
+
+
+
+@app.route('/list/historial_tarea/<id_tarea>', methods=['GET'])
+def get_historial_id_tarea(id_tarea):
+    respons = {}
+    respons['status'] = 'OK'
+    respons['ruta'] = '/list/historial/id_tarea'
+
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute('SELECT * FROM historial WHERE id_tarea="' + id_tarea + '"')
+        data_max = []
+        for row in cur.fetchall():
+            data_min = {}
+            data_min['id'] = row[0]
+            data_min['id_tarea'] = row[1]
+            data_min['time'] = row[2]
+            data_min['realizada'] = row[3]
+            data_max.append(data_min)
+    con.close()
+
+    respons['tareas'] = json.dumps(data_max)
+
+    respons = jsonify(respons)
+    respons.status_code = 201
+
+    return respons
 
 
 """
